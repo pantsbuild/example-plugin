18e51f4677d3abc821d92b77f7a21103599e752e
Eric Arellano <ericarellano@me.com>
2020-07-01 15:42:10 -0700

    Simplify setup_venv.sh more

--- build-support/python/setup_venv.sh
+++ build-support/python/setup_venv.sh
@@ -2,9 +2,8 @@
 # Copyright 2020 Pants project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-# A simple script to set up a virtual environment (venv) with all the Python dependencies used in
-# the project. This venv is useful for integrating with IDEs. See
-# https://pants.readme.io/docs/python-third-party-dependencies.
+# This script is useful to set up a virtual environment so that IDEs understand the Python code.
+# See https://pants.readme.io/docs/python-third-party-dependencies.
 
 PYTHON_BIN=python3.8
 VIRTUALENV=build-support/python/.venv
@@ -12,6 +11,4 @@ PIP="${VIRTUALENV}/bin/pip"
 
 "${PYTHON_BIN}" -m venv "${VIRTUALENV}"
 "${PIP}" install pip --upgrade
-"${PIP}" install -r <(
-  ./pants --no-pantsd dependencies --type=3rdparty ::
-)
+"${PIP}" install -r <(./pants dependencies --type=3rdparty ::)
