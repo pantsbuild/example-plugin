b27bc385659954cb726bbfe36d6ecf65d589cdd9
Eric Arellano <ericarellano@me.com>
2020-07-06 17:46:54 -0700

    PR feedback from Benjy

--- pants-plugins/examples/bash/register.py
+++ pants-plugins/examples/bash/register.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Pants project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from bash.target_types import BashTarget
+from examples.bash.target_types import BashTarget
 
 
 def rules():
