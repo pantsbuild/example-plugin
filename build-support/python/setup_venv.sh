f72a5dfb7d2bca833ca7e91cc3d92fb31d8a3257
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-07-07 22:49:52 -0700

    Add basic setup and `bash` target type (#1)
    This demonstrates https://pants.readme.io/v2.0/docs/plugins-overview#where-to-define-your-plugin and the Target API.
    
    * Creates `pants-plugins` folder and shows how to load the plugin.
    * Sets up Python backend and linters so that we can verify the correctness of our plugin.
    * Sets up CI.
    * Adds script to create a venv for the purpose of IDE integrations.
    * Adds `bash()` target type.
       - This is very simplistic for now. We an add later.

---
+++ build-support/python/setup_venv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Copyright 2020 Pants project contributors.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+# This script is useful to set up a virtual environment so that IDEs understand the Python code.
+# See https://pants.readme.io/docs/python-third-party-dependencies.
+
+PYTHON_BIN=python3.8
+VIRTUALENV=build-support/python/.venv
+PIP="${VIRTUALENV}/bin/pip"
+
+"${PYTHON_BIN}" -m venv "${VIRTUALENV}"
+"${PIP}" install pip --upgrade
+"${PIP}" install -r <(./pants dependencies --type=3rdparty ::)
