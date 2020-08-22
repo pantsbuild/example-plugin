96ec42cb11370e8980862cd010b5933967b90714
Eric Arellano <ericarellano@me.com>
2020-08-21 22:55:52 -0700

    Pants works correctly!
    `python -m venv` doesn't include pip when run in CI. So, we can't smoke test this in CI. Oh well.

--- build-support/python/setup_venv.sh
+++ build-support/python/setup_venv.sh
@@ -5,14 +5,10 @@
 # This script is useful to set up a virtual environment so that IDEs understand the Python code.
 # See https://www.pantsbuild.org/docs/python-third-party-dependencies.
 
-set -x
-
 PYTHON_BIN=python3.6
 VIRTUALENV=build-support/python/.venv
 PIP="${VIRTUALENV}/bin/pip"
 
 "${PYTHON_BIN}" -m venv "${VIRTUALENV}"
-ls -A "${VIRTUALENV}"
-ls -A "${VIRTUALENV}"/bin
 "${PIP}" install pip --upgrade
 "${PIP}" install -r <(./pants dependencies --type=3rdparty ::)
